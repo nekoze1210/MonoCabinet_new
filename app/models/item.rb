87f0e697e7defe867441f96685d7f64a4a24d244
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-04-09 23:57:49 +0900

    Itemモデル作成

---
+++ app/models/item.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :integer          not null, primary key
+#  item_name  :string(255)
+#  maker      :string(255)
+#  image_url  :text(65535)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_items_on_user_id  (user_id)
+#
+
+class Item < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409145037) do
+ActiveRecord::Schema.define(version: 20180409145330) do
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "item_name"
+    t.string "maker"
+    t.text "image_url"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
