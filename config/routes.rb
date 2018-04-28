0d40f60c408f4691a15f742dce7e4141a10d5c99
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-04-29 00:29:08 +0900

    ルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show, :edit, :update]  do
+    resources :items do
+      resources :locations
+      collection do
+        get 'search'
+        post 'tweet'
+      end
+    end
+  end
+  resources :rakuten_items do
+    collection do
+      get   'search'
+    end
+  end
+
+  root 'top#welcome'
+  resources :top
+  get '/help' => 'top#help'
+  get '/developer' => 'top#developer'
+  get '/contact' => 'top#contact'
 end
